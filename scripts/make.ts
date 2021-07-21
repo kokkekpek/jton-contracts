a88e080d3ab57731ac78c9badd9c2d9543d05918
Kok Kek <kokkekpek@gmail.com>
2021-07-21 08:30:10 +0300

    Setup compiler and linker versions in make script

--- scripts/make.ts
+++ scripts/make.ts
@@ -11,4 +11,7 @@ make({
         'src/tonlabs/SafeMultisigWallet/2020-05-04T16:46:21Z/contract/SafeMultisigWallet',
         'src/tonlabs/SetcodeMultisigWallet/2020-11-19T11:36:30Z/contract/SetcodeMultisigWallet'
     ],
+    compiler: '0.47.0',
+    linker: '0.11.84',
+    stdlib: '0.47.0'
 }).catch((e: any) => console.log(e))
