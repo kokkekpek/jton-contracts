8ba088b2ca908fa041bb37a36a622b4736a789c7
Kok Kek <kokkekpek@gmail.com>
2021-07-22 09:49:51 +0300

    Fix. Update make script.

--- scripts/make.ts
+++ scripts/make.ts
@@ -4,12 +4,13 @@ import {root} from '../root'
 make({
     root,
     compile: [
-        'src/kokkekpek/Idle/contract/Idle'
+        'src/kokkekpek/Idle/b911fc8f70d58477d53bf514e693bf76f96efd25/contract/Idle'
     ],
     wrap: [
-        'src/tonlabs/GiverV2/2021-04-28T09:41:28Z/contract/GiverV2',
-        'src/tonlabs/SafeMultisigWallet/2020-05-04T16:46:21Z/contract/SafeMultisigWallet',
-        'src/tonlabs/SetcodeMultisigWallet/2020-11-19T11:36:30Z/contract/SetcodeMultisigWallet'
+        'src/tonlabs/GiverV2/8a2bc005cfec4ecd770d50b074179e525b76513b/contract/GiverV2',
+        'src/tonlabs/GiverV2/06b351a91f104943ec10fed8ae262e54ecaae871/contract/GiverV2',
+        'src/tonlabs/SafeMultisigWallet/5ee039e4d093b91b6fdf7d77b9627e2e7d37f000/contract/SafeMultisigWallet',
+        'src/tonlabs/SetcodeMultisigWallet/c62e5855bd4af95ba842094f21419c74921f9e10/contract/SetcodeMultisigWallet'
     ],
     compiler: '0.47.0',
     linker: '0.11.84',
