68cfcf2a301a53fa349f61aa390dd3d8734fba01
Kok Kek <kokkekpek@gmail.com>
2021-07-22 09:55:53 +0300

    Separate Idle compiling and wrapping

--- scripts/make.ts
+++ scripts/make.ts
@@ -4,9 +4,10 @@ import {root} from '../root'
 make({
     root,
     compile: [
-        'src/kokkekpek/Idle/b911fc8f70d58477d53bf514e693bf76f96efd25/contract/Idle'
+        'src/kokkekpek/Idle/source/Idle'
     ],
     wrap: [
+        'src/kokkekpek/Idle/b911fc8f70d58477d53bf514e693bf76f96efd25/contract/Idle',
         'src/tonlabs/GiverV2/8a2bc005cfec4ecd770d50b074179e525b76513b/contract/GiverV2',
         'src/tonlabs/GiverV2/06b351a91f104943ec10fed8ae262e54ecaae871/contract/GiverV2',
         'src/tonlabs/SafeMultisigWallet/5ee039e4d093b91b6fdf7d77b9627e2e7d37f000/contract/SafeMultisigWallet',
